c489b9c8b5f9e368f598f04f7859437ef506d671
moraa121212 <108996785+moraa121212@users.noreply.github.com>
2025-04-19 03:39:47 +0200

    Create main.tf

---
+++ modules/route-table/main.tf
@@ -0,0 +1,19 @@
+resource "aws_default_route_table" "public" {
+  default_route_table_id = var.mora
+  tags = {
+    Name = "iti-default-route-public"
+  }
+}
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_default_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             =  var.gateway
+  timeouts {
+    create = "5m"
+  }
+
+}
+resource "aws_route_table_association" "public" {
+  subnet_id      = var.subnet_id
+  route_table_id = aws_default_route_table.public.id
+}
